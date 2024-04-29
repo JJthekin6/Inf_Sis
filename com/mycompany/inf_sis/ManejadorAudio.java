19ea45eea427ce09629defc9ee015fcbfbb42247
Aliere <allieig666@gmail.com>
2024-04-29 17:55:28 -0600

    Comentarios agregados

--- com/mycompany/inf_sis/ManejadorAudio.java
+++ com/mycompany/inf_sis/ManejadorAudio.java
@@ -23,12 +23,12 @@ public class ManejadorAudio {
     /* En realidad esta clase debió haber sido una interfaz porque nunca se instancía y todos sus
      * métodos son estáticos, but oh well... */
 
-    /* Formato de grabación: En esta variable se indica el formato que usaremos para abrir las lineas de
+    /* Formato de grabación: En esta variable se indica el formato que usaremos para abrir las líneas de
      * audio de entrada/salida. En este caso estamos usado los valores sugeridos para el formato WAV que
      * encontramos en: https://docs.fileformat.com/es/audio/wav/ */
     private static AudioFormat formatoGrabacion = new AudioFormat(44100, 16, 1, true, false);
 
-    /* Información de la linea de datos: Este es un objeto que necesitamos para poder crear las lineas de 
+    /* Información de la línea de datos: Este es un objeto que necesitamos para poder crear las líneas de 
      * audio más adelante, se crea usando el formato de grabación.*/
     private static DataLine.Info infoLinea = new DataLine.Info(TargetDataLine.class, formatoGrabacion);
 
@@ -37,13 +37,13 @@ public class ManejadorAudio {
      * implementado un administrador de archivos). */
     private static File archivo = new File("Grabación.wav");
 
-    /* Lineas de audio: Aquí solo se hace la declaración, pero estas son las lineas de audio que usaremos
+    /* Líneas de audio: Aquí solo se hace la declaración, pero estas son las líneas de audio que usaremos
      * más adelante.*/
-    private static TargetDataLine lineaGrabacion;  //Linea del microfonó principal del sistema
-    private static SourceDataLine lineaReproduccion; //Linea del altavoz principal del sistema
+    private static TargetDataLine lineaGrabacion;  //Línea del microfonó principal del sistema
+    private static SourceDataLine lineaReproduccion; //Línea del altavoz principal del sistema
 
     //M. INICIAR GRABACIÓN
-    /* Este es el método que se ejecuta cuando le picamos al botón de "grabar" en la llamada 2*/
+    /* Este es el método que se ejecuta cuando le picamos al botón de "Grabar" en la llamada 2*/
     public static void iniciarGrabacion() {
         //Bloque "try/catch" por si la línea está ocupada
         try {
@@ -65,13 +65,15 @@ public class ManejadorAudio {
                 System.out.println("Grabando...");
                 //Bloque "try/catch" por si hay un problema con la línea o al guardar el archivo
                 try {
-                    //Abrimos la linea de grabación con el formato que especificamos anteriormente
+                    //Abrimos la línea de grabación con el formato que especificamos anteriormente
                     lineaGrabacion.open(formatoGrabacion, lineaGrabacion.getBufferSize());
                     lineaGrabacion.start();
+
                     /* Creamos una transmición de audio a partir de la línea de grabación, este objeto
-                    /* maneja el flujo de bytes directo desde el micrófono */
+                    /* maneja el flujo de bytes directo desde el micrófono. */
                     AudioInputStream transmicionAudio = new AudioInputStream(lineaGrabacion);
-                    /* Usamos la transmición de audio para guardar la grabación en el archivo */
+
+                    //Usamos la transmición de audio para guardar la grabación en el archivo
                     AudioSystem.write(transmicionAudio, Type.WAVE, archivo);
                 } catch (Exception e) {
                     System.out.println("Error al grabar");
@@ -82,46 +84,64 @@ public class ManejadorAudio {
         //Creamos un hilo con el objeto "Runnable" que acabamos de crear y lo lanzamos
         new Thread(hiloGrabacion).start();
     }
-    
+
+    //M. DETENER GRABACIÓN
+    /* Este es el método que se ejecuta cuando le picamos al botón de "Detener" en la llamada 2 */
     public static void detenerGrabacion() {
-        //System.out.println("Presione enter para terminar de grabar");
-        //entrada.nextLine();
+        /* Solo tenemos que cerrar la línea de grabacion. Cuando la transmición de audio se quede sin
+         * bytes que mandar, el hilo de grabación se cierra solito. */
         lineaGrabacion.stop();
         lineaGrabacion.close();
-        System.out.println("Grabación finalizada\n");
+        System.out.println("Grabación finalizada\n"); //"\n" es un salto de línea, osea como picar enter
     }
 
+    //M. INICIAR REPRODUCCIÓN
+    /* Este es el método que se ejecuta cuando le picamos al botón de "Reproducir" en la llamada 3 */
     public static void iniciarReproduccion() {
-        AudioInputStream transmicionAudio;
+        AudioInputStream transmicionAudio; //Declaración de una transmición de audio
+        //Bloque "try/catch" por si hay un problema al leer el archivo
         try {
+            /* Creamos una transmición de audio a partir del archivo de grabación, este objeto almacenara
+             * los bytes de audio que leemos desde el archivo. */
             transmicionAudio = AudioSystem.getAudioInputStream(archivo);
         } catch (Exception e) {
             System.out.println("Error al leer el archivo");
             e.printStackTrace();
-            return;
+            return; //Si no se puede leer el archivo cancelamos la reproducción
         }
-        //System.out.println("Presione enter para reproducir");
-        //entrada.nextLine();
         
+        /* Actualizamos el objeto con la información de la línea para indicarle que ahora usaremos una
+         * línea de salida. Lo que cambia es que en el primer parametro ponemos "SourceDataLine". */
         infoLinea = new DataLine.Info(SourceDataLine.class, formatoGrabacion);
+        //Bloque "try/catch" por si la línea se encuentra ocupada
         try {
             lineaReproduccion = (SourceDataLine)AudioSystem.getLine(infoLinea);
         } catch (LineUnavailableException e) {
             System.out.println("Linea no disponible");
             e.printStackTrace();
         }
-        
+
+        /* Creamos otra implementación anónima de la interfaz "Runnable" para poder procesar la
+         * reproducción en un hilo separado. */
         Runnable hiloReproducción = new Runnable() {
+            //Método sobrescrito "run" que se ejecutará automaticamente cuando lancemos el hilo
             @Override
             public void run() {
                 System.out.println("Reproduciendo...");
+                //Bloque "try/catch" por si hay un problema con la línea o al reproducir el audio
                 try {
+                    //Abrimos la línea de reproducción con el formato que especificamos anteriormente
                     lineaReproduccion.open(formatoGrabacion);
                     lineaReproduccion.start();
-                    int tamañoBuffer = 4096;
-                    byte[] bufferDeBytes = new byte[tamañoBuffer];
-                    int bytesLeidos = -1;
+
+                    //Creamos un buffer de bytes para mandar los bytes del archivo en rondas
+                    int tamañoBuffer = 4096; //Tamaño del buffer
+                    byte[] bufferDeBytes = new byte[tamañoBuffer]; //Buffer como arreglo de bytes
+                    int bytesLeidos = -1; //Contador de bytes leídos
+
+                    //Cilco para mandar los bytes a la línea de reproducción
                     while ((bytesLeidos = transmicionAudio.read(bufferDeBytes)) != -1) {
+                        //Usamos el método "write" de la línea de reproducción para mandar el buffer
                         lineaReproduccion.write(bufferDeBytes, 0, bytesLeidos);
                     }
                 } catch (Exception e) {
@@ -130,15 +150,23 @@ public class ManejadorAudio {
                 }
             }
         };
+        //Creamos un hilo con el objeto "Runnable" que acabamos de crear y lo lanzamos
         new Thread(hiloReproducción).start();
     }
 
+    //M. DETENDER REPRODUCCIÓN
+    /* Este es el método que se ejecuta cuando le picamos al botón de "Detener" en la llamada 3 */
     public static void detenerReproduccion() {
+        /* Igual que con la grabación solo tenemos que cerrar la línea de reproducción y el hilo
+         * se cierra solito. */
         lineaReproduccion.stop();
         lineaReproduccion.close();
         System.out.println("Reproducción interrumpida\n");
     }
-    
+
+    //M. IMPRIMIR INFORMACIÓN DE AUDIO
+    /* No se utiliza en la aplicación pero aquí se puede obtener la información de los dispositivos de
+     * entrada y salida de audio del sistema. */
     public void imprimirInformacionAudio() {
         for (Type type : AudioSystem.getAudioFileTypes()) {
             System.out.println(type.toString());
